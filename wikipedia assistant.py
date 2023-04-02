c3fedd30c02ed327ad129bab86138e3b1d7710e2
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2023-04-02 23:08:49 +0500

    Create wikipedia assistant.py

---
+++ wikipedia assistant.py
@@ -0,0 +1,76 @@
+# pip install pyaudio
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+        print("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")  
+        print("Good Afternoon!")
+ 
+
+    else:
+        speak("Good Evening!") 
+        print("Good Afternoon!")
+         
+
+    speak("I am Ali Sir. Please tell me how may I help you")  
+    print("I am Ali Sir. Please tell me how may I help you")   
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        speak("Say that again please...")
+        print("Say that again please...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=5)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        else:
+            speak("Ask by saying, wikipedia tell about something") #something that you want to ask
+            print("No query matched")    
